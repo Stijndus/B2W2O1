972a9a4d1f5968dfbc3b64dbfc22064cdafcb4f2
Stijn <stijn@ardusit.nl>
2020-12-01 21:03:40 +0100

    AAAAAAAAAAAAA

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
 const duck = document.getElementById('duck');
 
 var move = 75;
+var hits = 1;
+var miss = 1;
 
 function fly(direction) {
     direction = direction.toLowerCase();
@@ -9,17 +11,15 @@ function fly(direction) {
     var top = document.getElementById('duck').offsetTop;
     var left = document.getElementById('duck').offsetLeft;
 
-    switch(direction){
+    switch (direction) {
         case 'n':
-            if (top <= 0) {
-            } else {
+            if (top <= 0) {} else {
                 top = top - move;
                 duck.style.top = top + "px";
             }
             break;
         case 'ne':
-            if (top <= 0 || left >= 1650) {
-            } else {
+            if (top <= 0 || left >= 1650) {} else {
                 left = left + move;
                 duck.style.left = left + "px";
                 top = top - move;
@@ -27,15 +27,13 @@ function fly(direction) {
             }
             break;
         case 'e':
-            if (left >= 1650) {
-            } else {
+            if (left >= 1650) {} else {
                 left = left + move;
                 duck.style.left = left + "px";
             }
             break;
         case 'se':
-            if (top >= 750 || left >= 1650) {
-            } else {
+            if (top >= 750 || left >= 1650) {} else {
                 left = left + move;
                 duck.style.left = left + "px";
                 top = top + move;
@@ -43,15 +41,13 @@ function fly(direction) {
             }
             break;
         case 's':
-            if (top >= 750) {
-            } else {
+            if (top >= 750) {} else {
                 top = top + move;
                 duck.style.top = top + "px";
             }
             break;
         case 'sw':
-            if (top >= 750 || left <= 0) {
-            } else {
+            if (top >= 750 || left <= 0) {} else {
                 left = left - move;
                 duck.style.left = left + "px";
                 top = top + move;
@@ -59,15 +55,13 @@ function fly(direction) {
             }
             break;
         case 'w':
-            if (left <= 0) {
-            } else {
+            if (left <= 0) {} else {
                 left = left - move;
                 duck.style.left = left + "px";
             }
             break;
         case 'nw':
-            if (top <= 0 || left <= 0) {
-            } else {
+            if (top <= 0 || left <= 0) {} else {
                 left = left - move;
                 duck.style.left = left + "px";
                 top = top - move;
@@ -79,10 +73,34 @@ function fly(direction) {
     }
 }
 
-function moveDuck(){
-    setInterval(function(){
+function moveDuck() {
+    setInterval(function () {
         fly(directions[Math.floor(Math.random() * directions.length)]);
     }, 500)
 }
 
 moveDuck();
+
+function score(hit) {
+    if (hits < 21 && miss < 21) {
+        switch (hit) {
+            case 0:
+                document.getElementById('hit').innerText = hits++;
+                break;
+            case 1:
+                document.getElementById('miss').innerText = miss++;
+                break;
+            default:
+                break;
+        }
+    }
+    if (hits == 21) {
+        document.getElementById("win").classList.remove("d-none");
+    } else if (miss == 21) {
+        document.getElementById("lose").classList.remove("d-none");
+    }
+}
+
+function restart(){
+    window.location.reload();
+}
